5a31665a43af0838c74f29d7a4ee3b80e3275a47
KevinJHP19 <huamanpintokevin@fpllefia.com>
2024-10-14 14:26:42 +0000

    practica6 ejercicio 1 hecho

---
+++ ejercicios/ejercicio6/index.js
@@ -0,0 +1,26 @@
+
+console.log('hola')
+//Ejercicio 1
+function generarnumerorandom() { 
+const nummaximo = document.querySelector('#num2').value
+
+const numminimo = document.querySelector('#num1').value
+
+console.log(nummaximo , numminimo)
+
+const numrandom = Math.floor(Math.random(numminimo) * nummaximo);
+
+console.log(numrandom);
+
+const divnumerorandom=document.querySelector('#numeroAleatorio'); 
+divnumerorandom.innerHTML = '';
+divnumerorandom.innerHTML= numrandom;
+
+}
+
+document.getElementById('generarnumero').addEventListener('click', function(event){
+    event.preventDefault();
+    generarnumerorandom();
+})
+
+//Ejercicio 2
